169d99161b55eccf00ca2427b5b2fe363f7bbd50
Zachary Rolens <zachrolens@yahoo.com>
2024-06-23 12:08:05 -0500

    Addition of first pseudocode

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+// Function creation to get rock paper or scissors
+// Create number variable
+//  Input random number between 0 and 90 into number variable
+//      If number variable is less than or equal to 30 then return rock
+//      If number variable is greater than 30 but less than or equal to 60 then return paper
+//      If number variable is greater than 60 but less than or equal to 90 then return scissors
+// Print the return variable
