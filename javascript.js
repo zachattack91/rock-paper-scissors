daf053567298c76e42e98a974eb7eb9786e7008c
Zachary Rolens <zachrolens@yahoo.com>
2024-06-23 13:27:13 -0500

    Finished first block of code to add function getComputerChoice

--- javascript.js
+++ javascript.js
@@ -1,7 +1,18 @@
 // Function creation to get rock paper or scissors
-// Create number variable
-//  Input random number between 0 and 90 into number variable
-//      If number variable is less than or equal to 30 then return rock
-//      If number variable is greater than 30 but less than or equal to 60 then return paper
-//      If number variable is greater than 60 but less than or equal to 90 then return scissors
-// Print the return variable
+function getComputerChoice() {
+// Create number random number variable
+    const num = Math.floor(Math.random() * 31);
+//      If number variable is less than or equal to 10 then return rock
+        if (num <= 10) {
+            console.log("ROCK");
+        }            
+//      If number variable is greater than 10 but less than or equal to 20 then return paper
+        else if (num > 10,  num <= 20) {
+            console.log("PAPER");
+        }
+//      If number variable is greater than 20 but less than or equal to 30 then return scissors
+        else {
+            console.log("SCISSORS");
+        }
+        // Print the return variable
+}
