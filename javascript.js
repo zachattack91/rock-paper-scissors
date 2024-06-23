e3d380cdb4959db4484b85016d25549948e1185f
Zachary Rolens <zachrolens@yahoo.com>
2024-06-23 17:21:34 -0500

    Edited the original code to return a variable instead of printing to console.log

--- javascript.js
+++ javascript.js
@@ -4,15 +4,18 @@ function getComputerChoice() {
     const num = Math.floor(Math.random() * 31);
 //      If number variable is less than or equal to 10 then return rock
         if (num <= 10) {
-            console.log("ROCK");
+            return "Rock";
         }            
 //      If number variable is greater than 10 but less than or equal to 20 then return paper
         else if (num > 10,  num <= 20) {
-            console.log("PAPER");
+            return "Paper";
         }
 //      If number variable is greater than 20 but less than or equal to 30 then return scissors
         else {
-            console.log("SCISSORS");
+            return "Scissors";
         }
         // Print the return variable
+    console.log("num");
 }
+
+// 
